77553b57f26597b1b58ce7404c391c79492a3291
urvish <urvishpvt@gmail.com>
2023-01-08 11:43:13 +0530

    ss

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -71,6 +71,13 @@ export default function Post({ post, socialImage, related }) {
     <Layout>
       <Head>
         <meta name="robots" content="noindex" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Helmet {...helmetSettings} />
 
